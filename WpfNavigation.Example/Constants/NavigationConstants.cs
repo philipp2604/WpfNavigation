cc1694ade4f2ee49d21517d1ecc94e2a6cef5472
Philipp Siewert <philipp@phsiewert.de>
2024-08-14 07:13:34 +0200

    Commented NavigationConstants

--- WpfNavigation.Example/Constants/NavigationConstants.cs
+++ WpfNavigation.Example/Constants/NavigationConstants.cs
@@ -5,14 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfNavigation.Example.Constants;
+
+/// <summary>
+/// Navigation constants.
+/// </summary>
 public static class NavigationConstants
 {
+    /// <summary>
+    /// Region keys.
+    /// </summary>
     public static class Regions
     {
         public const string Region1 = "Region1";
         public const string Region2 = "Region2";
     }
 
+    /// <summary>
+    /// Content keys.
+    /// </summary>
     public static class Content
     {
         public const string Content1 = "Content1";
